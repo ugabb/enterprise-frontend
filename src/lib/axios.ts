013963e505868e61e306accdd663663d8d0b1ce7
Gabriel Barros <ugabrieldev@gmai.com>
2024-05-17 08:48:16 -0300

    feat: adding custom delay to test skeleton loading

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,3 +4,9 @@ import { env } from "../env";
 export const api = axios.create({
   baseURL: "http://localhost:3333",
 });
+
+api.interceptors.request.use(async (config) => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return config;
+});
