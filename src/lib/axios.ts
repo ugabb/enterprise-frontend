cc18f223ce1a27020ecbbd8ef1db06f7b0d65fbb
Gabriel Barros <ugabrieldev@gmai.com>
2024-05-17 20:16:37 -0300

    feat: update axios base URL fallback to localhost

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333",
 });
+
 
 // api.interceptors.request.use(async (config) => {
 //   await new Promise((resolve) => setTimeout(resolve, 2000));
